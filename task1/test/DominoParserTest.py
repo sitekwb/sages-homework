0a452bd5edb3ab420bc92c49949369969460d860
Wojciech Sitek <s.wojciech9@gmail.com>
2022-07-21 08:41:52 +0200

    #1 Access src package from test

--- task1/test/DominoParserTest.py
+++ task1/test/DominoParserTest.py
@@ -1,6 +1,12 @@
 import unittest
 
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
+
+
 from DominoParser import DominoParser
+
 
 
 class DominoParserTest(unittest.TestCase):
